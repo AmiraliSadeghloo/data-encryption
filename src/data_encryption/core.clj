e22a65159c16e4d914f22a3a7c45bc06a3ac55b8
AmirAli Sadeghloo <amirali.sadeghloo@gmail.com>
2020-06-13 11:12:13 +0430

    last

--- src/data_encryption/core.clj
+++ src/data_encryption/core.clj
@@ -25,10 +25,10 @@
   (let [input (read)]
     input))
 
-(def input (inp))
+(def input (inp))                                           ;;remove
 (def inputCount (count input))
-(println input)
-(println inputCount)
+
+(println "input Count: " inputCount)
 ;;//////////////////////
 (def countToSquare                                          ;;returns min sqaurelist above inputcount[row square]
   (fn []
@@ -36,7 +36,7 @@
       (if (or (= (nth square i) inputCount) (> (nth square i) inputCount))
         [(inc i) (nth square i)]
         (recur (inc i) square)))))
-(countToSquare)
+
 ;;//////////////////////
 (def countToRectangle                                       ;;returns min rectanglelist above inputcount[row rectangle]
   (fn []
@@ -44,49 +44,49 @@
       (if (or (= (nth rectangle i) inputCount) (> (nth rectangle i) inputCount))
         [i (nth rectangle i)]
         (recur (inc i) rectangle)))))
-(countToRectangle)
+
 ;;//////////////////////
 (def whichOne                                               ;;if true -> square , false -> rectangle
   (fn []
     (true? (> (last (countToRectangle)) (last (countToSquare))))))
-(whichOne)
+
 ;;//////////////////////
 (def diff                                                   ;;difference between inputCount and (rectangle or square)
   (fn []
     (if (= true (whichOne))
       (- (last (countToSquare)) inputCount)
       (- (last (countToRectangle)) inputCount))))
-(diff)
+
 ;;//////////////////////
 (defn squareAdd []                                          ;; adds "" to input to make it square
   (loop [i (diff) newSquare input]
     (if (= i 0)
       newSquare
       (recur (dec i) (conj newSquare "fillfillfill")))))
-(squareAdd)
+
 ;;//////////////////////
 (defn rectangleAdd []                                       ;;adds "fillfillfill" to input to make it rectangle
   (loop [i (diff) newRectangle input]
     (if (= i 0)
       newRectangle
       (recur (dec i) (conj newRectangle "fillfillfill")))))
-(rectangleAdd)
+
 ;;//////////////////////
 (defn parting [whichOne]                                    ;;partitionates input based on rec/sqr
   (if (= true whichOne)
     (mapv vec (partition (first (countToSquare)) (squareAdd)))
     (mapv vec (partition (inc (first (countToRectangle))) (rectangleAdd)))))
-(parting whichOne)
+
 ;;//////////////////////
 (def saveLast (drop-last (diff) (last (parting whichOne)))) ;;saves last real row
-(println saveLast)
+
 ;;//////////////////////
 (def droping (drop-last 1 (parting whichOne)))              ;;drops last row
-(println droping)
+
 ;;//////////////////////
 (defn result []                                             ;;conj all = result
   (conj (into [] droping) (into [] saveLast)))
-(result)
+
 ;;//////////////////////
 (defn applyMap [result]                                     ;;maps all vectors cols
   (apply map vector result))
